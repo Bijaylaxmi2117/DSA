065d570f51815d7e9030367fe8a656f3c5976acb
Bijaylaxmi <beheramama098@gmail.com>
2021-12-28 07:39:22 -0500

    Lecture 5 class work added

---
+++ Lecture_05/prog_03.cpp
@@ -0,0 +1,15 @@
+//Print the count from 1 to n.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"Printing the count from 1 to "<<n<<endl;
+    for (int i = 1; i <=n; i++)
+    {
+        cout<<i<<" ";
+    }
+    
+    return 0;
+}
---
+++ Lecture_05/prog_04.cpp
@@ -0,0 +1,16 @@
+//Find the sum from 1 to n.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n :";
+    cin>>n;
+    int sum=0;
+    for (int i = 1; i <=n; i++)
+    {
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
