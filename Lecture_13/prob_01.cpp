c86f67d2e896a90e442b6888fa399baf996a7959
Bijaylaxmi Behera <72284560+Bijaylaxmi2117@users.noreply.github.com>
2022-01-30 11:49:04 -0500

    lecture_13 added

---
+++ Lecture_13/prob_01.cpp
@@ -0,0 +1,60 @@
+//Find the first and last position of element in a sorted array.
+#include<iostream>
+using namespace std;
+int firstOcc(int arr[],int n,int key)
+{
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while (s<=e)
+    {
+        if(arr[mid]==key)
+        {
+            ans=mid;
+            e=mid-1;
+        }
+        else if(key>arr[mid])
+        {
+            s=mid+1;
+        }
+        else if(key<arr[mid])
+        {
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+    
+}
+int lastOcc(int arr[],int n,int key)
+{
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while (s<=e)
+    {
+        if(arr[mid]==key)
+        {
+            ans=mid;
+            s=mid+1;
+        }
+        else if(key>arr[mid])
+        {
+            s=mid+1;
+        }
+        else if(key<arr[mid])
+        {
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+    
+}
+int main()
+{
+    int arr[8]={1,2,2,2,2,2,3,3};
+    cout<<"First occurence of 2 is at index "<<firstOcc(arr,8,2)<<endl;
+    cout<<"Last occurence of 2 is at index "<<lastOcc(arr,8,2)<<endl;
+    return 0;
+}
